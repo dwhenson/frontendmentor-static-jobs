ebf0d5212d8c6d1f904b9bffe837b997ce93ff4c
dwhenson <dave.henson@gmail.com>
2021-05-06 20:27:28 +0300

    🦟 FIX: Close icon in top filters

--- js/job-postings.js
+++ js/job-postings.js
@@ -66,10 +66,11 @@ function renderFilters(filtersToRender) {
 
 	activeFilters.innerHTML = filtersToRender
 		.map(function (filter) {
-			return `<label for="active-${filter}"
+			// prettier-ignore
+			return /* html */`<label for="active-${filter}"
 				>${filter}
 				<input data-filter="HTML" id="${filter}" type="checkbox" />
-				<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14"><path fill="#FFF" fill-rule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/></svg>
+				<img src="./images/icon-remove.svg" alt="remove">
 			</label>`;
 		})
 		.join("");
